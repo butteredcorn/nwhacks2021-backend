12ceda0fe4f7897fdf8f1adaf89ddffe3093633a
parsasi <parsasi@rocketmail.com>
2021-01-10 03:04:34 +0330

    database configuration added

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-module.exports = () => {
-    router.get('/' ,(req,res) => {
-        res.json('hello')
+module.exports = (database) => {
+    router.get('/' ,async (req,res) => {
+        // '70c9e0dd-c7c1-4cbf-8d6c-ec85523b1239'
+        const restaurant = await database.getRestaurants()
+        res.json(restaurant)
     })
    
 
