43eaee07fab06fbee5638d96937cd1483ba40cda
Chris <cng177@my.bcit.ca>
2021-01-09 17:24:46 -0800

    Add types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+interface MenuItem {
+  title: string
+  description: string
+  price: number
+}
+
+interface Table {
+  qr: string
+}
+
+export interface CreateRestaurant {
+  name: string
+  password: string
+  menu: MenuItem[]
+  tables: Table[]
+}
