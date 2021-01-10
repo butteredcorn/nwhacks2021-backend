1065906237c02639fd436802e4e1d890b216c4c8
Chris <cng177@my.bcit.ca>
2021-01-09 16:21:01 -0800

    Merge api.js into api/index.js

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,13 +3,15 @@ const router = express.Router()
 const restaurantRoute = require('./restaurant')()
 const purchaseRoute = require('./purchase')()
 
-module.exports = () => {
-    router.get('/' ,(req,res) => {
-        res.json('hello')
-    })
+module.exports = (database) => {
+  router.get('/', async (req,res) => {
+    // '70c9e0dd-c7c1-4cbf-8d6c-ec85523b1239'
+    const restaurant = await database.getRestaurants()
+    res.json(restaurant)
+  })
 
-    router.use('/restaurant', restaurantRoute)
-    router.use('/purchase', purchaseRoute)
+  router.use('/restaurant', restaurantRoute)
+  router.use('/purchase', purchaseRoute)
 
-    return router
+  return router
 }
